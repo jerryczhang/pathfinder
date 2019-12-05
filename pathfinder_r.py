1285d22da80aa1b111f4843aad2e3db3ae2cb78c
jerryczhang <jerrychzhang@gmail.com>
2019-12-04 20:56:53 -0800

    Clean up dead-end detection and behavior
    Removed previous mechanism for dead-end detection, because using the
    recursive method properly handles dead-end detection and behavior by
    default. Added a call to move() with each dead end detection, so the
    robot can backtrack to the last valid spot.

--- pathfinder_r.py
+++ pathfinder_r.py
@@ -28,7 +28,6 @@ def print_maze(maze, start, path):
     print("Current Node: " + str(start))
     for key, value in maze.items():
         print(str(key) + ' : ' + str(value))
-    print("invalid Nodes: " + str(invalid))
     print("path: " + str(path))
 
 def move(position, robot_pos):
@@ -51,14 +50,14 @@ def random_success(rate):
     """Returns true with the frequency provided by rate."""
     return randrange(100) <= rate * 100
 
-def get_end(manual_input=False, success_rate=0.01):
+def get_end(manual_input=False, sensor_input=False, success_rate=0.01):
     """Get input for whether the robot is at the end of the maze."""
-    if manual_input:
+    if manual_input or sensor_input:
         return input("End? (T/F):").lower() == 't'
     else:
         return random_success(success_rate)
 
-def scan(direction, manual_input=False, sensor_input=False, success_rate=0.4):
+def scan(direction, manual_input=False, sensor_input=False, success_rate=0.75):
     """Get input for whether a direction is valid. Can be manual or sensor input, or random."""
     if sensor_input:
         input("Scan " + direction)
@@ -66,9 +65,9 @@ def scan(direction, manual_input=False, sensor_input=False, success_rate=0.4):
     elif manual_input:
         return input(direction + " valid (T/F):").lower() == 't'
     else:
-        return random_succeses(success_rate)
+        return random_success(success_rate)
     
-def find_path(maze, start, invalid, robot_pos, path=[]):
+def find_path(maze, start, robot_pos, path=[]):
     """Recursively iterate through maze nodes, constructing and solving a graph.
 
     Parameters:
@@ -77,7 +76,6 @@ def find_path(maze, start, invalid, robot_pos, path=[]):
                     adjacent nodes
         start:      The starting node of the maze--this represents the current node with
                     each recursive call
-        invalid:    The list containing all nodes that lead to dead-ends 
         robot_pos:  The current position of the physical robot
         path:       The list of nodes that the robot has traveled through
 
@@ -90,15 +88,10 @@ def find_path(maze, start, invalid, robot_pos, path=[]):
     move(path[-1], robot_pos)
     if get_end(manual_input=True):
         return path 
-    if len(path) >= 2 and path[-1] == path[-2]:
-        invalid.append(path.pop())
-        if len(path) >= 2:
-            move(path[-2], robot_pos)
-        return
 
     for direction in maze[start]:
         nearby_node = maze[start][direction]
-        if not nearby_node:
+        if nearby_node == None:
             nearby_node_x = start[0]
             nearby_node_y = start[1]
             valid = scan(direction, manual_input=True)
@@ -122,51 +115,53 @@ def find_path(maze, start, invalid, robot_pos, path=[]):
                     nearby_node_x -= 1
                 nearby_node = (nearby_node_x, nearby_node_y)
                 maze[start][direction] = nearby_node
-                empty = {'n': False, 'e': False, 's': False, 'w': False, reverse(direction): path[-1]}
+                empty = {'n': None, 'e': None, 's': None, 'w': None, reverse(direction): path[-1]}
                 #create new node
                 maze[nearby_node] = empty
-                #update array
-                array_x = {10:1, 11:3, 12:5, 13:7}
-                array_y = {10:7, 11:5, 12:3, 13:1}
-                array_coor_x = array_x[start[0]]
-                array_coor_y = array_y[start[1]]
-                if len(path) > 1:
-                    prev_array_x = array_x[path[-2][0]]
-                    prev_array_y = array_y[path[-2][1]]
-                    array[prev_array_y][prev_array_x] = add_color(' ', "Yellow", highlight = True)
-                    array[array_coor_y][array_coor_x] = add_color(' ', "Green", highlight = True)
-                for direction in maze[start]:
-                    if maze[start][direction] == False:
-                        wall = add_color(' ', "Red", highlight  = True)
-                    else:
-                        wall = add_color(' ', "Teal", highlight = True)
-                    if direction == 'n':
-                        array[array_coor_y - 1][array_coor_x] = wall
-                    elif direction == 'e':
-                        array[array_coor_y][array_coor_x + 1] = wall
-                    elif direction == 's':
-                        array[array_coor_y + 1][array_coor_x] = wall
-                    elif direction == 'w':
-                        array[array_coor_y][array_coor_x - 1] = wall    
+            else:
+                maze[start][direction] = False
+            array_x = {10:1, 11:3, 12:5, 13:7}
+            array_y = {10:7, 11:5, 12:3, 13:1}
+            array_coor_x = array_x[start[0]]
+            array_coor_y = array_y[start[1]]
+            if len(path) > 1:
+                prev_array_x = array_x[path[-2][0]]
+                prev_array_y = array_y[path[-2][1]]
+                array[prev_array_y][prev_array_x] = add_color(' ', "Yellow", highlight = True)
+                array[array_coor_y][array_coor_x] = add_color(' ', "Green", highlight = True)
+            for direction in maze[start]:
+                if maze[start][direction] == False:
+                    wall = add_color(' ', "Red", highlight  = True)
+                else:
+                    wall = add_color(' ', "Teal", highlight = True)
+                if direction == 'n':
+                    array[array_coor_y - 1][array_coor_x] = wall
+                elif direction == 'e':
+                    array[array_coor_y][array_coor_x + 1] = wall
+                elif direction == 's':
+                    array[array_coor_y + 1][array_coor_x] = wall
+                elif direction == 'w':
+                    array[array_coor_y][array_coor_x - 1] = wall    
     #find next
     for direction in maze[start]:
-        if maze[start][direction] and maze[start][direction] not in path and maze[start][direction] not in invalid:
-            newpath = find_path(maze, maze[start][direction], invalid, robot_pos, path)
+        if maze[start][direction] and maze[start][direction] not in path:
+            newpath = find_path(maze, maze[start][direction], robot_pos, path)
             if newpath:
                 return newpath
+            else:
+                move(path[-1], robot_pos)
     return None
 
 array = construct_maze()
 maze = {(10, 10):
     {
-        'n': False,
-        'e': False,
-        's': False,
-        'w': False,
+        'n': None,
+        'e': None,
+        's': None,
+        'w': None,
     }
 }
-invalid = []
-path = find_path(maze, (10,10), invalid, [10, 10])
+path = find_path(maze, (10,10), [10, 10])
 print("=======================")
 if path:
     print("Finished, path: " + str(path))

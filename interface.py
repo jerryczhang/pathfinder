e720fc4a5c7a332990e26cc4e29958ff6aa15ba4
jerryczhang <jerrychzhang@gmail.com>
2019-12-10 14:15:20 -0800

    Merge better-display with master
    Add better display formatting. Resolve issue where robot would override
    information in a node when revisting that node.

--- interface.py
+++ interface.py
@@ -1,48 +1,84 @@
-#TODO implement numpy array
-from color import add_color
+import numpy as np
 import sys
 
-def construct_display():
-    """Constructs a 10 by 10 display representation of the blank maze."""
-    row = []
-    display = []
-    for _ in range(1, 10):
-        blank = str(add_color(' ', "Black", highlight = True))
-        row.append(blank)
-    for _ in range(1, 10):
-        display.append(list(row))
-    return display 
+class Display:
 
-def print_display(display):
-    """Print the maze in graphical form with color formatting."""
-    for row in display:
-        for item in row:
-            sys.stdout.write(item)
-        sys.stdout.write('\n')
+    def __init__(self):
+        """Initialize the text blocks used for display, as well as the np array."""
+        self.BLANK = "\033[40m  \033[0m"
+        self.WALL = "\033[47m  \033[0m"
+        self.CURR = "\033[40m[]\033[0m"
+        self.START = "\033[40m<>\033[0m"
+        self.PATH = "\033[40m::\033[0m"
+        self.display = np.full((3, 3), self.BLANK)
+        self.x_offset = 1
+        self.y_offset = 1
 
-def print_maze(maze, start, path):
-    """Print the text representation of the maze.""" 
-    print(''.center(20, '='))
-    print("Current Node: " + str(start))
-    for key, value in maze.items():
-        print(str(key) + ' : ' + str(value))
-    print("path: " + str(path))
+    def print_display(self):
+        """Print the maze in graphical form with color formatting."""
+        for row in self.display:
+            for item in row:
+                sys.stdout.write(item)
+            sys.stdout.write('\n')
 
-def update_display(maze, start, path, display):
-    """Update the graphical array."""
-    display_x = {10:1, 11:3, 12:5, 13:7}
-    display_y = {10:7, 11:5, 12:3, 13:1}
-    display_coor = (display_x[start[0]], display_y[start[1]])
-    for direction in maze[start]:
-        if maze[start][direction] == "invalid":
-            wall = add_color(' ', "Red", highlight  = True)
-        else:
-            wall = add_color(' ', "Teal", highlight = True)
+    def expand(self, direction):
+        """Expand the np array in a specified direction."""
+        (y, x) = self.display.shape
         if direction == 'n':
-            display[display_coor[1] - 1][display_coor[0]] = wall
-        elif direction == 'e':
-            display[display_coor[1]][display_coor[0] + 1] = wall
-        elif direction == 's':
-            display[display_coor[1] + 1][display_coor[0]] = wall
-        elif direction == 'w':
-            display[display_coor[1]][display_coor[0] - 1] = wall    
+            self.display = np.concatenate((np.full((2, x), self.BLANK), self.display), axis=0)
+            self.y_offset += 2
+        if direction == 'e':
+            self.display = np.concatenate((self.display, np.full((y, 2), self.BLANK)), axis=1)
+        if direction == 's':
+            self.display = np.concatenate((self.display, np.full((2, x), self.BLANK)), axis=0)
+        if direction == 'w':
+            self.display = np.concatenate((np.full((y, 2), self.BLANK), self.display), axis=1)
+            self.x_offset += 2
+
+    def add_element(self, node, element, offset_dirs=None):
+        """Add maze coordinates node to the display, offset in directions offset_dirs."""
+        x, y = self.transform(node)
+        if offset_dirs:
+            if 'n' in offset_dirs:
+                y -= 1
+            elif 's' in offset_dirs:
+                y += 1
+            if 'e' in offset_dirs:
+                x += 1
+            elif 'w' in offset_dirs:
+                x -= 1
+        if x > self.display.shape[1] - 1:
+            self.expand('e')
+        if y > self.display.shape[0] - 1:
+            self.expand('s')
+        if x < 0:
+            self.expand('w')
+            x += 2
+        if y < 0:
+            self.expand('n')
+            y += 2
+        self.display[y][x] = element
+
+    def transform(self, node):
+        """Converts maze coordinates into array coordinates."""
+        xt = 2 * node[0] + self.x_offset
+        yt = 2 * node[1] + self.y_offset
+        return xt, yt
+
+    def update_display(self, path, maze):
+        """Update the graphical array."""
+        curr_node = path[-1]
+        for direction in maze[curr_node]:
+            if maze[curr_node][direction] == "invalid":
+                self.add_element(curr_node, self.WALL, direction)
+        for offset_dir in ['ne', 'se', 'sw', 'nw']:
+            self.add_element(curr_node, self.WALL, offset_dir)
+        for node in maze:
+            if node == path[0]:
+                self.add_element(node, self.START)
+            elif node == path[-1]:
+                self.add_element(node, self.CURR)
+            elif node in path:
+                self.add_element(node, self.PATH)
+            else:
+                self.add_element(node, self.BLANK)
--- pathfinder.py
+++ pathfinder.py
@@ -1,23 +1,21 @@
 import interface
 from random import randrange
+from time import sleep
 
-MANUAL_INPUT = True
+MANUAL_INPUT = False
 SENSOR_INPUT = False
-RANDOM_INPUT = False
+RANDOM_INPUT = True
 
 def move(position, robot_pos):
     """Moves the robot to position from robot_pos."""
     xdist = position[0] - robot_pos[0]
     ydist = position[1] - robot_pos[1]
-    if xdist >= 2 or ydist >= 2:
-        return
-    else:
-        robot_pos[0] += xdist
-        robot_pos[1] += ydist
-        print("Robot position: " + str(robot_pos))
+    robot_pos[0] += xdist
+    robot_pos[1] += ydist
+    print("Robot position: " + str(robot_pos))
 
 def reverse(direction):
-    """reverse the given compass direction (n, e, s, w)."""
+    """Reverses the given compass direction (n, e, s, w)."""
     directions = ['n', 'e', 's', 'w', 'n', 'e', 's', 'w']
     return directions[directions.index(direction) + 2]
 
@@ -25,14 +23,15 @@ def random_success(rate):
     """Returns true with the frequency provided by rate."""
     return randrange(100) <= rate * 100
 
-def get_end(manual_input=False, sensor_input=False, success_rate=0.01):
+def get_end(path, maze, success_rate=0.01):
     """Get input for whether the robot is at the end of the maze."""
-    if manual_input or sensor_input:
+    if MANUAL_INPUT or SENSOR_INPUT:
         return input("End? (T/F):").lower() == 't'
     else:
+        sleep(0.1)
         return random_success(success_rate)
 
-def scan(direction, manual_input=False, sensor_input=False, success_rate=0.75):
+def scan(direction, manual_input=False, sensor_input=False, success_rate=0.4):
     """Get input for whether a direction is valid. Can be manual or sensor input, or random."""
     if sensor_input:
         input("Scan " + direction)
@@ -46,11 +45,11 @@ def get_nearby_node(direction, current_node):
     """Get the coordinates of the adjacent node."""
     adj = list(current_node)
     if direction == 'n':
-        adj[1] += 1
+        adj[1] -= 1
     elif direction == 'e':
         adj[0] += 1
     elif direction == 's':
-        adj[1] -= 1
+        adj[1] += 1
     elif direction == 'w':
         adj[0] -= 1
     return tuple(adj) 
@@ -71,30 +70,33 @@ def find_path(maze, start, robot_pos, path, display):
         The path to the end of the maze, if it exists, and None otherwise
     """
     path = path + [start]
-    interface.print_display(display)
-    interface.print_maze(maze, start, path)
+    display.update_display(path, maze)
+    display.print_display()
     move(path[-1], robot_pos)
-    if get_end(manual_input=MANUAL_INPUT, sensor_input=SENSOR_INPUT):
+    if get_end(path, maze):
         return path 
 
     for direction in maze[start]:
-        adj = get_nearby_node(direction, start)
-        if adj not in maze or maze[adj][reverse(direction)] == "unknown":
-            valid = scan(direction, manual_input = MANUAL_INPUT, sensor_input = SENSOR_INPUT)
-            if valid:
-                maze[start][direction] = adj
-                maze[adj] = {
-                        'n': "unknown", 
-                        'e': "unknown", 
-                        's': "unknown", 
-                        'w': "unknown", 
-                        reverse(direction): path[-1]
-                }
-            else:
+        if maze[start][direction] == "unknown":
+            adj = get_nearby_node(direction, start)
+            if adj not in maze or maze[adj][reverse(direction)] == "unknown":
+                valid = scan(direction, manual_input = MANUAL_INPUT, sensor_input = SENSOR_INPUT)
+                if valid:
+                    maze[start][direction] = adj
+                    maze[adj] = {
+                            'n': "unknown", 
+                            'e': "unknown", 
+                            's': "unknown", 
+                            'w': "unknown", 
+                            reverse(direction): path[-1]
+                    }
+                else:
+                    maze[start][direction] = "invalid"
+            elif maze[adj][reverse(direction)] == "invalid":
                 maze[start][direction] = "invalid"
-        elif maze[adj][reverse(direction)] == "invalid":
-            maze[start][direction] = "invalid"
-        interface.update_display(maze, start, path, display)
+            else:
+                maze[start][direction] = adj
+            display.update_display(path, maze)
     for direction in maze[start]:
         if maze[start][direction] != "invalid" and maze[start][direction] not in path:
             newpath = find_path(maze, maze[start][direction], robot_pos, path, display)
@@ -106,8 +108,8 @@ def find_path(maze, start, robot_pos, path, display):
 
 def main():
     """Initializes maze and display, then calls find_path."""
-    display = interface.construct_display()
-    maze = {(10, 10):
+    display = interface.Display()
+    maze = {(0, 0):
         {
             'n': "unknown",
             'e': "unknown",
@@ -115,7 +117,7 @@ def main():
             'w': "unknown",
         }
     }
-    path = find_path(maze, (10,10), [10, 10], [], display)
+    path = find_path(maze, (0,0), [0, 0], [], display)
     print(''.center(20, '='))
     if path:
         print("Finished, path: " + str(path))

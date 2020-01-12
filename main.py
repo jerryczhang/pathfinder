0b401831f60e0e7b10156ce0012fe2ead017f996
jerryczhang <jerrychzhang@gmail.com>
2020-01-11 20:26:22 -0800

    Add time measure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #from robot import Robot
 #import RPi.GPIO as GPIO
 from pathfinder import Pathfinder
+from time import time
 
 MANUAL = 0
 RANDOM = 1
@@ -13,15 +14,18 @@ def main():
     #GPIO.setmode(GPIO.BCM)
     #GPIO.setwarnings(False)
 
-    pathfinder = Pathfinder(MANUAL, (3, -3), 1)
+    pathfinder = Pathfinder(MANUAL, (3, -3), 0)
     while True:
         x_start = int(input("X coordinate of starting node:"))
         y_start = int(input("Y coordinate of starting node:"))
+        time0 = time()
         path = pathfinder.start((x_start, y_start), "mazes/maze1.txt")
         if path:
             print("Finished, path: " + str(path))
         else:
             print("Impossible maze")
+        time1 = time()
+        print("Time: " + str(time1 - time0))
     #GPIO.cleanup()
 
 if __name__ == '__main__':
